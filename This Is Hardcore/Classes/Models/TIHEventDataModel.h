530a7ba01cb41c46224295e5a17fe64e1cbb2eff
Kevin Clough <kevinclough@gmail.com>
2012-06-15 15:16:41 -0400

    Added Event Data Model

---
+++ This Is Hardcore/Classes/Models/TIHEventDataModel.h
@@ -0,0 +1,13 @@
+//
+//  TIHEventDataModel.h
+//  This Is Hardcore
+//
+//  Created by Kevin Clough on 6/15/12.
+//  Copyright (c) 2012 appRenaissance. All rights reserved.
+//
+
+#import "TIHBaseDataModel.h"
+
+@interface TIHEventDataModel : TIHBaseDataModel
+
+@end
---
+++ This Is Hardcore/Classes/Models/TIHEventDataModel.m
@@ -0,0 +1,13 @@
+//
+//  TIHEventDataModel.m
+//  This Is Hardcore
+//
+//  Created by Kevin Clough on 6/15/12.
+//  Copyright (c) 2012 appRenaissance. All rights reserved.
+//
+
+#import "TIHEventDataModel.h"
+
+@implementation TIHEventDataModel
+
+@end
