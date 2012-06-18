fb86f73430b125971c1a53678d9471bf623e9d4e
Odin-CI <odin@apprenaissance.com>
2012-06-18 16:17:43 -0400

    Added Notification Manager, and additional date class to ARiOSUtilities

---
+++ This Is Hardcore/Classes/TIHNotificationManager.h
@@ -0,0 +1,16 @@
+//
+//  TIHNotificationManager.h
+//  This Is Hardcore
+//
+//  Created by Odin on 6/18/12.
+//  Copyright (c) 2012 appRenaissance. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TIHEventDataModel.h"
+
+@interface TIHNotificationManager : NSObject
+
+- (void)scheduleNotificationWithEvent:(TIHEventDataModel *)event;
+
+@end
---
+++ This Is Hardcore/Classes/TIHNotificationManager.m
@@ -0,0 +1,37 @@
+//
+//  TIHNotificationManager.m
+//  This Is Hardcore
+//
+//  Created by Odin on 6/18/12.
+//  Copyright (c) 2012 appRenaissance. All rights reserved.
+//
+
+#import "TIHNotificationManager.h"
+
+@implementation TIHNotificationManager
+
+- (void)scheduleNotificationWithEvent:(TIHEventDataModel *)event {
+    NSDate *itemDate = [event startTime];
+    NSString *message = [event artistName];
+    
+    UILocalNotification *localNotif = [[UILocalNotification alloc] init];
+    if (localNotif == nil)
+        return;
+
+    localNotif.fireDate = [itemDate dateByAddingTimeInterval:-(300)];
+    localNotif.timeZone = [NSTimeZone defaultTimeZone];
+    
+    localNotif.alertBody = [NSString stringWithFormat:NSLocalizedString(@"%@ in %i minutes.", nil),
+                            message, 5];
+    localNotif.alertAction = NSLocalizedString(@"View Event", nil);
+    
+    localNotif.soundName = UILocalNotificationDefaultSoundName;
+    
+    NSDictionary *infoDict = [NSDictionary dictionaryWithObject:message forKey:
+                                [NSNumber numberWithInt:[event eventId]]];
+    localNotif.userInfo = infoDict;
+    
+    [[UIApplication sharedApplication] scheduleLocalNotification:localNotif];
+}
+
+@end
