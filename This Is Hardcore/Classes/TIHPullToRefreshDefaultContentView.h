af9645eea670321eaa173c6631e8885836123b43
Kevin Clough <kevinclough@gmail.com>
2012-07-19 10:35:12 -0400

    Aligned instagram tag display label with image

---
+++ This Is Hardcore/Classes/TIHPullToRefreshDefaultContentView.h
@@ -0,0 +1,13 @@
+//
+//  TIHPullToRefreshDefaultContentView.h
+//  This Is Hardcore
+//
+//  Created by Kevin Clough on 7/19/12.
+//  Copyright (c) 2012 appRenaissance. All rights reserved.
+//
+
+#import "SSPullToRefreshDefaultContentView.h"
+
+@interface TIHPullToRefreshDefaultContentView : SSPullToRefreshDefaultContentView
+
+@end
---
+++ This Is Hardcore/Classes/TIHPullToRefreshDefaultContentView.m
@@ -0,0 +1,31 @@
+//
+//  TIHPullToRefreshDefaultContentView.m
+//  This Is Hardcore
+//
+//  Created by Kevin Clough on 7/19/12.
+//  Copyright (c) 2012 appRenaissance. All rights reserved.
+//
+
+#import "TIHPullToRefreshDefaultContentView.h"
+
+@implementation TIHPullToRefreshDefaultContentView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
